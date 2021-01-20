7d96b0dab04acc8f72fbd2b1b00bb7f6acceeca7
ZHang Chi <605357670@qq.com>
2021-01-20 13:52:42 +0800

    6

---
+++ filesys.py
@@ -0,0 +1,62 @@
+import time
+
+from watchdog.events import *
+from watchdog.observers import Observer
+
+import configparser
+import os
+
+from threading import Thread
+import subprocess
+
+class FileEventHandler(FileSystemEventHandler):
+    def __init__(self):
+        FileSystemEventHandler.__init__(self)
+
+    def on_moved(self, event):
+        if event.is_directory:
+            print("目录移动从{0}到{1}".format(event.src_path, event.dest_path))
+        else:
+            print("文件移动从{0}到{1}".format(event.src_path, event.dest_path))
+
+    def on_created(self, event):
+        if event.is_directory:
+            print("目录创建:{0}".format(event.src_path))
+        else:
+            print("文件创建:{0}".format(event.src_path))
+
+    def on_deleted(self, event):
+        if event.is_directory:
+            print("目录删除:{0}".format(event.src_path))
+        else:
+            print("文件删除:{0}".format(event.src_path))
+
+    def on_modified(self, event):
+        if event.is_directory:
+            pass
+        else:
+            print("文件修改:{0}".format(event.src_path))
+            
+conf= configparser.ConfigParser()
+def readConf():
+    '''读取配置文件'''
+    conf.read('config.conf')  # 文件路径
+    list = conf.get("directory","pathlist").split(',')  # 获取指定section 的option值
+    return list
+
+if __name__ == "__main__":
+    pathlist = readConf()
+    for path in pathlist:
+        observer1 = Observer()
+        event_handler = FileEventHandler()
+
+        observer1.schedule(event_handler, path , True)
+        observer1.start()
+     
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer1.stop()
+    observer1.join()
+    
---
+++ out.py
@@ -0,0 +1,41 @@
+from scapy.all import *
+from subprocess import *
+import configparser
+def warn(a):
+	global whitelist
+	#print(a.summary())
+	if a.haslayer(DNS):
+		pass
+	else:	
+		#print(a[TCP].sport)
+		#net=subprocess.check_output('netstat -nap | grep {}'.format(a[TCP].sport),shell=True).decode()
+		#print('###'+net)
+		if a.haslayer(TCP):
+			try:
+				net=subprocess.check_output('lsof -i | grep {}'.format(a[TCP].sport), shell=True).decode()
+			except:
+				#print("\033[1;31;1m"+'Error:'+"\033[0m"+' Fail to get app name on TCP port '+"\033[1;31;1m"+str(a[IP].sport)+"\033[0m")
+				pass
+			else:
+				if net.split(' ')[0] not in whitelist:
+					print('Warning: '+"\033[1;31;1m"+net.split(' ')[0]+"\033[0m"+' has sent a TCP message to '+"\033[1;31;1m"+str(a[IP].dst)+"\033[0m."+' on port '+"\033[1;31;1m"+str(a.sport)+"\033[0m.")
+		else:
+			try:
+				net=subprocess.check_output('lsof -i | grep {}'.format(a[UDP].sport), shell=True).decode()
+			except:
+				#print("\033[1;31;1m"+'Error:'+"\033[0m"+' Fail to get app name on UDP port '+"\033[1;31;1m"+str(a[IP].sport)+"\033[0m")
+				pass
+			else:
+				if net.split(' ')[0] not in whitelist:
+					print('Warning: '+"\033[1;31;1m"+net.split(' ')[0]+"\033[0m"+' has sent a UDP message (not DNS) to '+"\033[1;31;1m"+str(a[IP].dst)+"\033[0m."+' on port '+"\033[1;31;1m"+str(a[IP].sport)+"\033[0m.")
+	return  
+
+conf= configparser.ConfigParser()
+conf.read('config.conf')  # 文件路径
+whitelist = conf.get("whitelist","applist").split(',')  # 获取指定section 的option值
+#with open('config.conf','r') as f:
+#	whitelist=f.readlines()[0].split()
+print('White List: '+str(whitelist))
+local='192.168.12.137'
+sniff(filter='ip src {}'.format(local),prn=warn)
+#sniff(filter='ip src {}'.format(local),prn=lambda x : x[IP])
